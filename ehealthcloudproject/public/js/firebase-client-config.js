609aaf1767827919cb2ab808884360e9f19b8354
Bhavineka <bhavineka5@gmail.com>
2025-11-09 08:23:52 +0530

    second

--- ehealthcloudproject/public/js/firebase-client-config.js
+++ ehealthcloudproject/public/js/firebase-client-config.js
@@ -6,6 +6,8 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// firebase-client-config.js - COMPAT VERSION (for your existing auth.js)
+
 const firebaseConfig = {
   apiKey: "AIzaSyCpp55ITZSKlJjVHWBkrubYPY1BVFH2UcM",
   authDomain: "ehealthcloudproject.firebaseapp.com",
@@ -15,6 +17,9 @@ const firebaseConfig = {
   appId: "1:607633042601:web:8f35f5cfd05e04b10d0db8",
   measurementId: "G-FMY8FHN74Y"
 };
+
+// This file just defines the config - initialization happens in auth.js
+// No need to initialize here since auth.js does it
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
